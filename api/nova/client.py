fdcca3ecd39d82fe174a52630f025092701d2a9b
somuthegamer49 <78402611+somuthegamer49@users.noreply.github.com>
2026-03-15 17:43:37 +0530

    Update client.py

--- api/nova/client.py
+++ api/nova/client.py
@@ -7,6 +7,6 @@ load_dotenv()
 bedrock = boto3.client(
     service_name="bedrock-runtime",
     region_name=os.getenv("AWS_REGION"),
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_KEY")
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
 )
