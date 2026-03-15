eafb4f791841f371ef9f32e3db92a81fb766ff8a
somuthegamer49 <78402611+somuthegamer49@users.noreply.github.com>
2026-03-15 18:54:30 +0530

    Update Centerarea.js

--- client/src/components/Centerarea.js
+++ client/src/components/Centerarea.js
@@ -49,7 +49,7 @@ const Centerarea = () => {
       const postToBackend = async () => {
         setworking("Working...");
         await axios
-          .post("https://noble-app.vercel.app/agent", payload, {
+          .post("https://noble-app.vercel.app/api/agent", payload, {
             headers: {
               "Content-Type": "application/json",
             },
