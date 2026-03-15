c8ae474c051b213968a133842959f84be24c8268
somuthegamer49 <78402611+somuthegamer49@users.noreply.github.com>
2026-03-15 18:00:32 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 import json
 
 app = Flask(__name__)
-CORS(app, origins=["https://noble-app.vercel.app/"])
+CORS(app, origins=["https://noble-app.vercel.app"])
 
 @app.route("/agent", methods=["POST"])
 def agent():
