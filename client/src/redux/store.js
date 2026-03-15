840c321b574eaecdd7a486eedf559e4aae14113e
somuthegamer49 <78402611+somuthegamer49@users.noreply.github.com>
2026-03-15 21:32:19 +0530

    Delete client directory

--- client/src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { legacy_createStore as createStore } from 'redux'; // Use legacy_createStore
-import stateReducer from './reducer';
-
-const store = createStore(stateReducer);
-
-export default store;
